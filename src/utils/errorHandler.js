9824201d522063ac2f52a69e455819ff0f987e64
youyomyom0498-alt <youyomyom@gmail.com>
2025-10-19 20:33:53 +0900

    fix(errorHandler): 예외 처리 부분에서 에러 던지도록 오류 수정

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,5 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export function handleError(error) {
-    Console.print(`[ERROR] ${error.message}`);
+    throw new Error(`[ERROR] ${error.message}`);
 }
